5b9e3aa2d30fc0561178a3f81a8009b94cea74ff
Vital Fadeev <vital.fadeev@gmail.com>
2020-06-06 11:48:47 +0700

    comment

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ rem SET PATH=C:\src\panel\work\result\bin;%PATH%
 rem 64 on 64
 rem set LINKCMD64=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\link.exe
 
-rem 64 on 32
+rem 32 on 64
 rem set LINKCMD64=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x86\link.exe
 
 rem build 64
